8c9c6482aab33ba9f9dd329fdd31b8883cc72d8a
Alexander <fenaethylaamiinloever@gmail.com>
2023-05-23 14:38:52 +0300

    .gitidnore added, acc modified

--- js/script.js
+++ js/script.js
@@ -45,23 +45,43 @@ $(function () {
 
 })
 
-//new acc
-let accHeadings = document.querySelectorAll('.questions__acc-heading');
-let lines = document.querySelectorAll('.questions__acc-line--vertical');
-let accItems = document.querySelectorAll('.questions__acc-item');
+//old acc
+// let accHeadings = document.querySelectorAll('.questions__acc-heading');
+// let lines = document.querySelectorAll('.questions__acc-line--vertical');
+// let accItems = document.querySelectorAll('.questions__acc-item');
 
-for (let i = 0; i < accHeadings.length; i++) {
-	accHeadings[i].addEventListener('click', function () {
+// for (let i = 0; i < accHeadings.length; i++) {
+// 	accHeadings[i].addEventListener('click', function () {
 
-		accItems[i].classList.toggle('question__acc-item--active');
+// 		accItems[i].classList.toggle('question__acc-item--active');
 
-		lines[i].classList.toggle('questions__acc-line--active');
-		let text = this.nextElementSibling;
-		if (text.style.maxHeight) {
-			text.style.maxHeight = null;
-		} else {
-			text.style.maxHeight = text.scrollHeight + "px";
+// 		lines[i].classList.toggle('questions__acc-line--active');
+// 		let text = this.nextElementSibling;
+// 		if (text.style.maxHeight) {
+// 			text.style.maxHeight = null;
+// 		} else {
+// 			text.style.maxHeight = text.scrollHeight + "px";
+// 		}
+// 	});
+// }
+
+let links = document.querySelectorAll('.questions__acc-heading');
+
+for (let link of links) {
+	link.addEventListener('click', function (e) {
+		e.preventDefault();
+
+		let open = document.querySelector('.questions__acc-item--active');
+
+		if (this.parentElement.parentElement.classList.contains('questions__acc-item--active')) {
+			this.parentElement.parentElement.classList.remove('questions__acc-item--active');
 		}
-	});
+		else {
+			this.parentElement.parentElement.classList.add('questions__acc-item--active');
+			if (open) {
+				open.classList.remove('questions__acc-item--active');
+			}
+		}
+	})
 }
 
